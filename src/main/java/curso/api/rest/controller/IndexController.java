6d0b51aff23145b7e2ccb1dd8e8608962b8b7a27
unknown <gustavonarede1@gmail.com>
2022-10-26 23:08:56 -0300

    commitando a parte da criacao de consulta de usuario

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -1,15 +1,38 @@
 package curso.api.rest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import curso.api.rest.model.Usuario;
+import curso.api.rest.repository.UsuarioRepository;
+
 @RestController
+@RequestMapping(value = "/usuario")
 public class IndexController {
 
-	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init() {
-		return new ResponseEntity("Ola REST Spring Boot", HttpStatus.OK);
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> init(@PathVariable(value = "id")Long id) {
+		
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+		
+	}
+	@GetMapping(value = "/", produces = "/application/json")
+	public ResponseEntity<List<Usuario>> usuario(){
+		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
+		
+		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
 	}
 }
