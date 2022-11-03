c5887150061d7327dce7fece19ec49318f406529
unknown <gustavonarede1@gmail.com>
2022-11-02 21:43:44 -0300

    comitando a aula2.26

--- src/main/java/curso/api/rest/model/Usuario.java
+++ src/main/java/curso/api/rest/model/Usuario.java
@@ -1,11 +1,16 @@
 package curso.api.rest.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Usuario implements Serializable {
@@ -24,6 +29,9 @@ public class Usuario implements Serializable {
 	private String senha;
 	
 	private String nome;
+	
+	@OneToMany(mappedBy= "usuario",orphanRemoval = true, cascade = CascadeType.ALL)
+	private List<Telefone> telefones = new ArrayList<Telefone>();
 
 	public Long getId() {
 		return id;
@@ -55,6 +63,16 @@ public class Usuario implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	
+
+	public List<Telefone> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(List<Telefone> telefones) {
+		this.telefones = telefones;
 	}
 
 	@Override
